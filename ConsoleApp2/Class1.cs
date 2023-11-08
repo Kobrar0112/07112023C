acd601601b34453215f256501d64ba0ca9aee47c
Kobrar0112 <85014618+Kobrar0112@users.noreply.github.com>
2023-11-08 16:13:28 +0300

    Class1.cs

--- ConsoleApp2/Class1.cs
+++ ConsoleApp2/Class1.cs
@@ -1,4 +1,4 @@
-﻿using System;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -8,11 +8,25 @@ namespace ConsoleApp1
 {
     internal class Human
     {
-        public string Name;
-        public int Age;
-        public string[] MyColor;
+        public string Name = "Абрамов Роман";
+        public int Age = 16;
+        public string [] MyColor = { "Зелёный", "Синий" };
     }
-    
+
+    internal class Human1
+    {
+        public string Name = "Абрамов Роман";
+        public int Age = 16;
+        public string[] MyColor = { "Зелёный", "Синий" };
+    }
+
+    internal class Human2
+    {
+        public string Name = "Абрамов Роман";
+        public int Age = 16;
+        public string[] MyColor = { "Зелёный", "Синий" };
+    }
+
 
 
 
