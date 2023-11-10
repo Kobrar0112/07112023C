eb23e5133476a78a5c98732f9e3e7d8ad032728a
Kobrar0112 <85014618+Kobrar0112@users.noreply.github.com>
2023-11-10 08:41:51 +0300

    Class1.cs

--- ConsoleApp2/Class1.cs
+++ ConsoleApp2/Class1.cs
@@ -6,21 +6,21 @@ using System.Threading.Tasks;
 
 namespace ConsoleApp1
 {
-    internal class Human
+    public class Human
     {
         public string Name = "Абрамов Роман";
         public int Age = 16;
         public string [] MyColor = { "Зелёный", "Синий" };
     }
 
-    internal class Human1
+    public class Human1
     {
         public string Name = "Абрамов Роман";
         public int Age = 16;
         public string[] MyColor = { "Зелёный", "Синий" };
     }
 
-    internal class Human2
+    public class Human2
     {
         public string Name = "Абрамов Роман";
         public int Age = 16;
